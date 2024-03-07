dff63a21e6d75c63bcbbfe032d409538aaa241c5
cesar angulo <cesarangulo@cesars-Mac-Studio.local>
2024-03-07 14:12:10 -0800

    implementing extende token api

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import { useEffect, useState } from "react";
 import banner from './assets/homepage_banner.gif'
 import viteLogo from '/vite.svg'
 import './App.css'
+import extjs from './ic/extjs.js';
+const api = extjs.connect('https://icp0.io/');
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const[listings, setListings] = useState([])
+
+  useEffect(()=>{
+
+  },[listings])
+
 
   useEffect(() => {
-    (async () =>
-      await initJuno({
-        satelliteId: "zggvx-3aaaa-aaaal-adxyq-cai"
-      }))();
+    (async () => {
+      console.log("before calling the api")
+      let listings = await api.token('gq5kt-4iaaa-aaaal-qdhuq-cai').listings();
+      console.log("result",listings)
+      setListings(listings)
+    } 
+    )();
   }, []);
 
   return (
@@ -25,6 +38,11 @@ function App() {
           count is {count}
         </button>
       </div>
+      {listings.map((item)=>{
+        <div>
+          {item}
+        </div>
+      })}
     </>
   )
 }
