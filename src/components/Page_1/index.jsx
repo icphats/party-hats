94dcda41febd7c126c4a0cfa6d34d4bad4811923
theo <thehotelcar@gmail.com>
2024-03-18 11:54:21 -0700

    added and updated banner images

--- src/components/Page_1/index.jsx
+++ src/components/Page_1/index.jsx
@@ -1,7 +1,9 @@
 import "./index.css"
+import banner from "../../assets/phats_banner_transp.gif"
 
 function Page_1() {
   return <div className="splash-container">
+    <img className="front-banner" src={banner} alt="image" />
 </div>;
 }
 
