48e2e2bee8efcc775854ba888d07a91264788813
theo <thehotelcar@gmail.com>
2024-03-19 10:53:33 -0700

    updated name of logo

--- src/components/Page_2/stats.jsx
+++ src/components/Page_2/stats.jsx
@@ -29,7 +29,7 @@ function Stats() {
 
   return <div className="stats-brand-container">
     <div className="brand-container">
-        <img src="../../src/assets/avatar_1000x1000.png" className="brand-logo" alt="icphats logo" />
+        <img src="../../src/assets/logo_1000x1000.png" className="brand-logo" alt="icphats logo" />
         <h1>IC Party Hats</h1>
         <a href="https://twitter.com/icphats" className="social_links" target="_blank" rel="noopener noreferrer">
             <img src="../../src/assets/x_logo.png" alt="Follow us on Twitter" style={{ width: '24px', height: '24px' }} />
