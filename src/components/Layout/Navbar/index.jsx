35f0cddb4bc8ee57979a51204d9a769d8f834e0f
theo <thehotelcar@gmail.com>
2024-03-19 11:04:10 -0700

    updated keyframe for logo

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -2,7 +2,7 @@ import "./index.css"
 
 function Navbar() {
     return <div className="navbar-container">
-        <img src="../../src/assets/avatar_1000x1000.png" className="brand-logo" alt="icphats logo" />
+        <img src="../../src/assets/logo_1000x1000.png" className="brand-logo" alt="icphats logo" />
         <h1>IC Party Hats</h1>
     </div>
 }
