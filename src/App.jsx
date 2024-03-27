434d75a5b9249340db7ddd1a75ce0965649ea241
theo <thehotelcar@gmail.com>
2024-03-27 15:42:24 -0700

    frontend password

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Principal } from "@dfinity/principal";
 import Landing from "./pages/landing";
 //import splash from "../assets/splash.png"
@@ -8,13 +8,44 @@ const api = extjs.connect("https://icp0.io/");
 const partyhatscanister = "gq5kt-4iaaa-aaaal-qdhuq-cai";
 import logo from "./assets/logo_1000x1000.png"
 
+
 function App() {
-  // if (!loaded) return <div className="loading-container">
-  //   <div className="loading-inner-content-container">
-  //     <img className="loading-logo" src={logo} alt="" />
-  //     <p className="loading-text df-font">Built on the Internet Computer</p>
-  //   </div>
-  // </div>;
+  
+  const [showPasswordInput, setShowPasswordInput] = useState(false);
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPasswordInput(true);
+  };
+
+  const handlePasswordChange = (event) => {
+    const password = event.target.value;
+    if (password === 'icphats') {
+      setIsPasswordCorrect(true);
+      setShowPasswordInput(false); // Optionally hide the input again
+    }
+  };
+
+
+
+  if (!isPasswordCorrect) return <div className="loading-container">
+    <div className="loading-inner-content-container">
+      {!showPasswordInput &&
+        (<>
+          <img onClick={handleButtonClick} className="loading-logo" src={logo} alt="" />
+          <p className="loading-text">Built on the Internet Computer</p>
+        </>)}
+
+      {showPasswordInput &&
+        (<input
+          type="password"
+          placeholder="Enter Password"
+          className="password-input-box"
+          onChange={handlePasswordChange}
+        />)
+      }
+    </div>
+  </div>;
 
   return (
     <div className="content">
