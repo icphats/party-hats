a0767b9b8d8c8e21f9ceaffc52d657e52e93038a
cesar angulo <cesarangulo@cesars-Mac-Studio.local>
2024-04-09 20:08:31 -0700

    juno.config.js

--- juno.config.js
+++ juno.config.js
@@ -4,6 +4,10 @@ import {defineConfig} from '@junobuild/config';
 export default defineConfig({
   satellite: {
     id: 'zggvx-3aaaa-aaaal-adxyq-cai',
-    source: 'dist'
+    source: 'dist',
+    "storage":{
+      "headers":[
+        ["Content-Security-Policy", "default-src 'self';script-src 'self' 'unsafe-eval';connect-src 'self' https://icp0.io https://*.icp0.io;img-src 'self' data:;style-src * 'unsafe-inline';style-src-elem * 'unsafe-inline';font-src *;object-src 'none';base-uri 'self';frame-ancestors 'self' https://desktop.windoge98.com https://windoge98.com ;form-action 'self';upgrade-insecure-requests;"]]
+    }
   }
 });
